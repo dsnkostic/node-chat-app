a4920d8559a24fb205e50d86825542285c63e4be
Dusan Kostic <dusan.kostic@primeholding.com>
2017-03-03 13:12:17 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Ben',
-    text: 'I am here.'
-  });
 });
 
 socket.on('disconnect', function () {
