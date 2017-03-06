68046c1a4554013ba55a498635e7c32e1056bef8
Dusan Kostic <dusan.kostic@primeholding.com>
2017-03-06 11:42:58 +0100

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+var moment = require('moment');
+
+var createdAt = 1234;
+var date = moment(createdAt);
+// date.add(1, 'year').subtract(9, 'months');
+// console.log(date.format('Do, MMM YYYY'));
+
+console.log(date.format('h:mm a'));
+
+// var date = new Date();
+//
+// console.log(date.getMonth());
